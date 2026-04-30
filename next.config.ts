bf565f972c30bb9dcbef71325aa823c32c52a5df
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2026-04-30 18:07:41 +0530

    fix llop and build error

--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,6 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-initOpenNextCloudflareForDev();
+if (process.env.NODE_ENV === "development") {
+  initOpenNextCloudflareForDev();
+}
